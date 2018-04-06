bf9dabc0e20c53f4201fffde4a119b61e0778b0f
Nicolas Pieter <nicolas@digitalfolks.sg>
2018-04-07 03:24:53 +0700

    Added main router

---
+++ src/routes.js
@@ -0,0 +1,19 @@
+import responder from 'Components/Responder.js';
+// import userRoute from './../containers/Users/userRoute.js';
+
+
+export default (app) => {
+
+	app.get('/', (req, res) => {
+		res.status(200).send('T H I C C');
+	});
+
+	// Insert other routes here
+	// userRoute(app);
+
+	// Catch all route
+	app.all('*', (req, res, next) => {
+		responder(res, next, { status: 404, data: 'Endpoint does not exist' });
+	});
+
+};
