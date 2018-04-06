853ea8f069d09fe27af4f7d783fe9baac6f3c11f
Nicolas Pieter <nicolas@digitalfolks.sg>
2018-04-07 03:23:14 +0700

    Added alias to root import

--- webpack.config.js
+++ webpack.config.js
@@ -30,8 +30,9 @@ module.exports = {
 
   resolve: {
     alias: {
-      Components: path.resolve(__dirname, '/src/components'),
-      Config: path.resolve(__dirname, '/src/config')
+      Components: path.resolve(__dirname, 'src/components/'),
+      Containers: path.resolve(__dirname, 'src/containers/'),
+      Config: path.resolve(__dirname, 'src/config/')
     }
   },
 
