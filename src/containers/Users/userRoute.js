a8927e638daec6bcf175f4f40ff246fb8710ed0c
Nicolas Pieter <nicolas@digitalfolks.sg>
2018-04-07 21:52:19 +0700

    Shortened code

--- src/containers/Users/userRoute.js
+++ src/containers/Users/userRoute.js
@@ -1,23 +1,12 @@
-import responder from './../../components/Responder.js';
+import responder from 'Components/Responder.js';
 import userController from './userController.js';
 
 export default (app) => {
 
 	app.post('/users', (req, res, next) => {
 		userController.createUser(app, req.body)
-			.then(response => {
-				// console.log(response)
-				responder(res, next, response);
-			})
-			.catch(error => {
-				// console.log('fdasfs')
-				console.log(error)
-				responder(res, next);
-			});
+			.then(response => responder(res, next, response))
+			.catch(error => responder(res, next, error));
 	});
-
-	// app.all('*', (req, res, next) => {
-	// 	responder(res, next, { status: 404, data: 'Endpoint does not exist' });
-	// });
 
 };
