ea5c7143f92a546e9b556c45040feff0cb99870d
Nicolas Pieter <nicolas@digitalfolks.sg>
2018-04-08 03:09:55 +0700

    Modified user schema to check for unique emails

--- src/containers/Users/userSchema.js
+++ src/containers/Users/userSchema.js
@@ -1,15 +1,30 @@
 import mongoose from 'mongoose';
+import uniqueValidator from 'mongoose-unique-validator';
 
-const schema = new mongoose.Schema({
-	username: {
-		type: String, required: true 
+
+const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+const userSchema = new mongoose.Schema({
+	email: {
+		type: String,
+		required: true,
+		validate: {
+			validator: (v) => { return emailRegex.test(v); },
+			message: '{VALUE} is not a valid email address!'
+		},
+		unique: true
 	},
 	password: {
-		type: String, required: true 
+		type: String,
+		required: true
 	},
 	dateCreated: {
-		type: Date, default: Date.now
+		type: Date,
+		default: Date.now
 	}
 });
 
-export default mongoose.model('User', schema);
+userSchema.plugin(uniqueValidator);
+
+
+export default mongoose.model('User', userSchema);
