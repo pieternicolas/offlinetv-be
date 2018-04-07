a08a33c52094c49376bdde76278d8f401ff7c1b5
Nicolas Pieter <nicolas@digitalfolks.sg>
2018-04-08 03:09:30 +0700

    Enabled user creation validator

--- src/containers/Users/userRoute.js
+++ src/containers/Users/userRoute.js
@@ -1,10 +1,12 @@
 import responder from 'Components/Responder.js';
 import userController from './userController.js';
+import userValidator from './userValidator.js';
+
 
 export default (app) => {
 
-	app.post('/users', (req, res, next) => {
-		userController.createUser(app, req.body)
+	app.post('/users', userValidator, (req, res, next) => {
+		userController.createUser(req)
 			.then(response => responder(res, next, response))
 			.catch(error => responder(res, next, error));
 	});
