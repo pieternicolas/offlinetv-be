034bcfdeab5848728c478b4ebc968278774b382a
Nicolas Pieter <nicolas@digitalfolks.sg>
2018-04-08 03:10:19 +0700

    Error checking and matched data check on controller

--- src/containers/Users/userController.js
+++ src/containers/Users/userController.js
@@ -1,8 +1,19 @@
+import { validationResult } from 'express-validator/check';
+import { matchedData } from 'express-validator/filter';
+
 import userModel from './userModel.js';
 
-export const createUser = (app, data) => {
+
+export const createUser = (data) => {
 	return new Promise ((resolve, reject) => {
-		userModel.createUser(data)
+		const errors = validationResult(data);
+		if (!errors.isEmpty()) {
+			reject({ status: 422, data: errors.mapped() });
+			return;
+		};
+
+		const user = matchedData(data);
+		userModel.createUser(user)
 		.then(res => {
 			if (!res) {
 				resolve({ status: 404 });
@@ -11,9 +22,10 @@ export const createUser = (app, data) => {
 			};
 		})
 		.catch(err => {
-			reject(err);
+			reject({ status: 400, data: err });
 		});
 	});
 };
+
 
 export default { createUser };
