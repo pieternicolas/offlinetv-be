2f504c37df9244a90a16a4db9ffdfbb48de302e9
Nicolas Pieter <nicolas@digitalfolks.sg>
2018-04-07 18:36:09 +0700

    Changed how to handle error

--- src/components/Responder.js
+++ src/components/Responder.js
@@ -8,14 +8,14 @@ export default (res, next, param) => {
 			break;
 
 		case 404:
-			res.status(status).json({ message: data ? data : 'Item not found' });
+			res.status(status).json(data ? data : { error: 'Item not found' });
 			break;
 
 		case 409:
-			res.status(status).json({ message: data ? data : 'Conflict detected' });
+			res.status(status).json(data ? data : { error: 'Conflict detected' });
 			break;
 
 		default:
-			res.status(status).json({ message: data ? data : 'Internal server error' });
+			res.status(status).json(data ? data : { error: 'Internal server error' });
 	};
 };
